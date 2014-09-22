35618242f063d43551808ac97f1bb3f3eb87b5b6
Johanna <jweinsziehr@gmx.de>
2014-09-22 14:38:43 -0700

    fixing

--- accounts/settings.py
+++ accounts/settings.py
@@ -67,7 +67,7 @@ EMAIL_PORT = 587
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE':'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
